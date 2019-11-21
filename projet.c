2693963a454528e5fcfb3b7203ebaeb9f8237b9f
iamELG <guilloton.elvin@gmail.com>
2019-11-21 11:30:28 +0100

    amélioration de l'IA

--- projet.c
+++ projet.c
@@ -1,16 +1,60 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+//direction
 #define N 78
 #define E 69
 #define S 83
 #define W 87
+//color
+#define GRN   "\x1B[32m"
+#define MAG   "\x1B[35m"
+#define BLU   "\x1B[34m"
+#define RESET "\x1B[0m"
 
+typedef struct token {
+	int aX;
+	int aY;
+	char aPlayer;
+}Tokens;
+
+Tokens generer_token(int pX, int pY){
+	Tokens elem;
+	elem.aX=pX;
+	elem.aY=pY;
+	elem.aPlayer='_';
+	return elem;
+}
+
+Tokens token_Hasard(int n, char plateau[n][n]){
+	Tokens t=generer_token(rand()%n,rand()%n);
+	if(plateau[t.aX][t.aY]!='_')
+		return token_Hasard(n,plateau);
+	return t;
+}
 
 void affichePlateau(int n, char plateau[n][n]){
+	
+	for(int i=0; i < n; ++i){
+		if(i==0)
+			printf(BLU "  |"RESET);
+		if(i<10)
+			printf(BLU " %d|"RESET,i);
+		if(i>=10)
+			printf(BLU "%d|"RESET,i);
+	}
+		
+	printf("\n");
 	for(int i=0;i<n;++i){
-	printf("%d  ",i);
+		if(i<10)
+			printf(BLU " %d|"RESET,i);
+		if(i>=10)
+			printf(BLU "%d|"RESET,i);
+		//debut de ligne
 		for(int j=0;j<n;++j){
-			printf("%c ",plateau[i][j]);
+
+			printf(" %c|",plateau[i][j]);
 		}
 		printf("\n");
 	}
@@ -46,6 +90,12 @@ int valideCoord(int x, int y, int n){
 	return 1;
 }
 
+int emplacementVide(int n, char plateau[n][n], int x, int y){
+	if(plateau[x][y]=='_')
+		return 1;
+	return 0;
+}
+
 int alignement(int x, int y, char direction[3], int n, char plateau[n][n], int nbAlignement){
 	int dX,dY;
 	convertitDirection(direction,&dX,&dY);
@@ -56,6 +106,12 @@ int alignement(int x, int y, char direction[3], int n, char plateau[n][n], int n
 	}
 	return 1;
 }
+
+int testAlignement(int x, int y, int n, char plateau[n][n], int nbAlignement){
+	return alignement(x,y,"N",n,plateau,nbAlignement) || alignement(x,y,"E",n,plateau,nbAlignement) || alignement(x,y,"S",n,plateau,nbAlignement) || 
+		   alignement(x,y,"W",n,plateau,nbAlignement) || alignement(x,y,"NE",n,plateau,nbAlignement) || alignement(x,y,"SE",n,plateau,nbAlignement) || 
+		   alignement(x,y,"SW",n,plateau,nbAlignement) || alignement(x,y,"NW",n,plateau,nbAlignement);
+}
 	
 void placeJoueur(int n, char plateau[n][n]){
 	int x,y;
@@ -63,8 +119,8 @@ void placeJoueur(int n, char plateau[n][n]){
 	scanf("%d",&x);
 	printf("y = ");
 	scanf("%d",&y);
-	while(!valideCoord(x,y,n)){
-		printf("Coordonnees impossibles ! Veuillez saisir de nouvelles coordonnees 0<x,y<%d :\nx = ",n-1);
+	while(!valideCoord(x,y,n) || !emplacementVide(n,plateau,x,y)){
+		printf("Coordonnees impossibles ! Veuillez saisir de nouvelles coordonnees 0<=(x,y)<=%d :\nx = ",n-1);
 		scanf("%d",&x);
 		printf("y = ");
 		scanf("%d",&y);
@@ -72,23 +128,12 @@ void placeJoueur(int n, char plateau[n][n]){
 	plateau[x][y]='O';
 }
 
-int testAlignement(int x, int y, int n, char plateau[n][n], int nbAlignement){
-	return alignement(x,y,"N",n,plateau,nbAlignement) || alignement(x,y,"E",n,plateau,nbAlignement) || alignement(x,y,"S",n,plateau,nbAlignement) || 
-		   alignement(x,y,"W",n,plateau,nbAlignement) || alignement(x,y,"NE",n,plateau,nbAlignement) || alignement(x,y,"SE",n,plateau,nbAlignement) || 
-		   alignement(x,y,"SW",n,plateau,nbAlignement) || alignement(x,y,"NW",n,plateau,nbAlignement);
-}
-
-int gagner(int n, char plateau[n][n], int nbAlignement){
+int gagnerAux(int n, char plateau[n][n], int nbAlignement, char joueur){
 	for(int i=0;i<n;++i){
 		for(int j=0;j<n;++j){
-			if(plateau[i][j]!='_'){
+			if(plateau[i][j]==joueur){
 				if(testAlignement(i,j,n,plateau,nbAlignement)){
-					if(plateau[i][j]=='X'){
-						printf("\nVous avez perdu !");
-						return -1000;
-					}
-					printf("\nVous avez gagné !");
-					return 1000;
+					return 1;
 				}
 			}
 		}
@@ -96,18 +141,147 @@ int gagner(int n, char plateau[n][n], int nbAlignement){
 	return 0;
 }
 
+int gagner(int n, char plateau[n][n], int nbAlignement){
+	if(gagnerAux(n,plateau,nbAlignement,'X')==1){
+		printf("\nVous avez perdu !");
+		return -1000;
+	}
+	if(gagnerAux(n,plateau,nbAlignement,'O')==1){
+		printf("\nVous avez gagné !");
+		return 1000;
+	}
+	return 0;
+}
+
+void placeHasardAux(int n, char plateau[n][n], Tokens *t){
+	int x=rand()%n;
+	int y=rand()%n;
+	while(!emplacementVide(n,plateau,x,y)){
+		x=rand()%n;
+		y=rand()%n;
+	}
+	plateau[x][y]='X';
+	(*t).aX=x;
+	(*t).aY=y;
+}
+
+
+int possible(int n, char plateau[n][n], int nbAlignement, Tokens *t){
+	for(int i=(*t).aX-nbAlignement+1;i<(*t).aX+nbAlignement;i++){
+		for(int j=(*t).aX-nbAlignement+1;j<(*t).aX+nbAlignement;j++){
+			if(plateau[i][j]=='_')
+				return 1;
+		}
+	}
+	return 0;
+}
+
+void placeHasard(int n, char plateau[n][n], Tokens *t, int nbAlignement){
+	if((*t).aX==-1){
+		placeHasardAux(n,plateau,&(*t));
+		return;
+	}
+	if(possible(n,plateau,nbAlignement,&(*t))){
+		int x=(*t).aX-nbAlignement+1+rand()%(2*nbAlignement-1);
+		int y=(*t).aX-nbAlignement+1+rand()%(2*nbAlignement-1);
+		while(!emplacementVide(n,plateau,x,y)){
+			x=(*t).aX-nbAlignement+1+rand()%(2*nbAlignement-1);
+			y=(*t).aX-nbAlignement+1+rand()%(2*nbAlignement-1);
+		}
+		plateau[x][y]='X';
+		printf("%d %d\n", x,y);
+		(*t).aX=x;
+		(*t).aY=y;
+		return;
+
+	}
+	placeHasardAux(n,plateau,&(*t));
+}
+
+
+
+int placeIAAux(int n, char plateau[n][n], int nbAlignement, char joueur1, char joueur2, Tokens *t){
+	for(int i=0;i<n;++i){
+		for(int j=0;j<n;++j){
+			if(plateau[i][j]=='_'){
+				plateau[i][j]=joueur1;
+				if(gagnerAux(n,plateau,nbAlignement,joueur1)==1){
+					*t=generer_token(i,j);
+					return 1;
+				}
+				plateau[i][j]=joueur2;
+				if(gagnerAux(n,plateau,nbAlignement,joueur2)==1){
+					plateau[i][j]=joueur1;
+					return 2;
+				}
+				plateau[i][j]='_';
+			}
+		}
+	}
+	return 3;
+}
+
+void placeIA(int n, char plateau[n][n], int nbAlignement, Tokens *tok){
+	Tokens t=generer_token(-1,-1);
+	if(placeIAAux(n,plateau,nbAlignement,'X','O',&t)==3){ //coup de l'ia n1
+		for (int i = 0; i < n; ++i){
+			for (int j = 0; j < n; ++j){
+				if(plateau[i][j]=='_'){
+					plateau[i][j]='X';
+					if(placeIAAux(n,plateau,nbAlignement,'X','O',&t)==1){ //pion placé on arrete
+						plateau[t.aX][t.aY]='_';
+						(*tok).aX=i;
+						(*tok).aY=j;
+						return;
+					}
+					if(placeIAAux(n,plateau,nbAlignement,'O','X',&t)==3){//on regarde si le joueur quand est ce que le joueur peut gagner au tour d'apres qu'il joue
+						plateau[i][j]='O';
+						if(placeIAAux(n,plateau,nbAlignement,'O','X',&t)==1){
+							plateau[i][j]='_';
+							plateau[t.aX][t.aY]='X';
+							(*tok).aX=t.aX;
+							(*tok).aY=t.aY;							
+							return;
+						}
+					}
+					plateau[i][j]='_';
+				}
+			}
+		}
+		placeHasard(n,plateau,&(*tok),nbAlignement);
+	}
+}
+
 int main(){
-	char plateau[10][10];
-	remplitAleatoire(10,plateau);
+	srand(time(NULL));
+	Tokens tok=generer_token(-1,-1);
+	int n=0;
+	int alignement=0;
+	while(n<3 || n>15){
+		printf("choisir la taille du plateau:");
+		scanf("%d",&n);
+	}
+	while(alignement<2 || alignement>n){
+		printf("choisir un nombre de pion a alignement:");
+		scanf("%d",&alignement);
+	}
+	char plateau[n][n];
+	remplitAleatoire(n,plateau);
 	char direction[3]="NW";
 	int x,y;
 	convertitDirection(direction,&x,&y);
-	affichePlateau(10,plateau);
-	while(gagner(10,plateau,5)==0){	
-		placeJoueur(10,plateau);
-		affichePlateau(10,plateau);
+	affichePlateau(n,plateau);
+	while(gagner(n,plateau,alignement)==0){	
+		placeJoueur(n,plateau);
+		affichePlateau(n,plateau);
+		if(gagner(n,plateau,alignement)!=0)
+			break;
+		sleep(1);
+		placeIA(n,plateau,alignement,&tok);
+		affichePlateau(n,plateau);
 	}
 	
 	
 	return 0;
 }
+
